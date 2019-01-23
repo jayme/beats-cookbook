2e23f7f6b89909380c4eaa3d7d56f6e8aeadcd64
jayme <jayme.poyner@gmail.com>
2019-01-23 14:11:27 -0500

    test kitchen updates

---
+++ test/cookbooks/beats_test/metadata.rb
@@ -0,0 +1,4 @@
+name 'beats_test'
+version '0.0.1'
+license 'GPL'
+depends 'beats'
---
+++ test/cookbooks/beats_test/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: beats
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+beats_install 'default'
+
+beats_configure 'default'
